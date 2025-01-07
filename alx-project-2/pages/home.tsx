54198355ef933db4115734eecf75dda153802435
Fraol-X <pfresh641@gmail.com>
2025-01-07 19:35:16 +0300

    Create a Reusable Card Component

--- alx-project-2/pages/home.tsx
+++ alx-project-2/pages/home.tsx
@@ -1,4 +1,11 @@
+import Card from '../components/common/Card';
+
 export default function HomePage() {
-    return <h1>Welcome to the Home Page</h1>;
-  }
-  
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Card title="Card 1" content="This is the first card." />
+      <Card title="Card 2" content="This is the second card." />
+    </div>
+  );
+}
